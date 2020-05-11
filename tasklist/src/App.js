900e9f0dcc45af8923f6728375a623a4a5ded77f
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-11 17:23:28 -0300

    feat: testando as configurações de eslint

--- tasklist/src/App.js
+++ tasklist/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <header className="App-header">
-      <h1>Task List</h1>
-    </header>
-  );
+    return (
+        <header className="App-header">
+            <h1>Task List</h1>
+        </header>
+    );
 }
 
 export default App;
