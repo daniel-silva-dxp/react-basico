cf4948cdc26f8ae87476359f1e740f223b2d1fc4
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-12 11:58:34 -0300

    feat: implementando component header

---
+++ tasklist/src/components/header/header.js
@@ -0,0 +1,14 @@
+"user strict";
+import React from "react";
+
+import "./header.css";
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <h1>My task list</h1>
+        </header>
+    );
+};
+
+export default Header;
