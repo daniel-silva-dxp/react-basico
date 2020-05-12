4a1f1916894832cdf0057f273fe824f4cb5374ee
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-12 11:59:37 -0300

    feat: implementando component header

---
+++ tasklist/src/components/main.js
@@ -0,0 +1,40 @@
+"user strict";
+import React, { Component } from "react";
+import Button from "./forms/button";
+import Input from "./forms/input";
+import Header from "./header/header";
+
+import "./main.css";
+
+class Main extends Component {
+    constructor() {
+        super();
+        this.state = {
+            newTask: ""
+        };
+    }
+    render() {
+        return (
+            <div className="main">
+                <Header />
+                <div className="container box">
+                    <form action="#">
+                        <Input
+                            newType="text"
+                            handleChange={e => {
+                                this.setState({
+                                    newTask: e.target.value
+                                });
+                            }}
+                            msg="Your Task"
+                        />
+                        <Button method="submit">ADD</Button>
+                        <div>{this.state.newTask}</div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
