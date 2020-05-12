35d1037384285930ce2c3c893f6e59f798e1e8a9
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-12 17:14:24 -0300

    feat: reestruturando component input

--- tasklist/src/components/forms/input.js
+++ tasklist/src/components/forms/input.js
@@ -4,14 +4,7 @@ import React from "react";
 import "./input.css";
 
 const Input = ({ handleChange, newType, msg }) => {
-    return (
-        <input
-            type={newType}
-            onChange={handleChange}
-            placeholder={msg}
-            className="form-control"
-        />
-    );
+    return <input type={newType} onChange={handleChange} placeholder={msg} />;
 };
 
 export default Input;
