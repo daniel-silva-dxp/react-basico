2fb87818e7a37b6ca58c3e7c97fd5404d1381887
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-13 15:03:35 -0300

    feat: implementado list no component main

--- tasklist/src/components/main.js
+++ tasklist/src/components/main.js
@@ -1,39 +1,79 @@
 "user strict";
 import React, { Component } from "react";
-import Button from "./forms/button";
-import Input from "./forms/input";
 import Header from "./header/header";
-import { FaPlus } from "react-icons/fa";
+import { FaEdit, FaWindowClose } from "react-icons/fa";
 
 import "./main.css";
+import Form from "./forms/form";
 
 class Main extends Component {
     constructor() {
         super();
         this.state = {
-            newTask: ""
+            newTask: "",
+            tasks: [
+                "Estudar Reactjs",
+                "Estudar Inglês",
+                "Estudar para certificação"
+            ]
         };
     }
+
     render() {
         return (
             <div className="main">
                 <Header />
-                <div className="container box">
-                    <form action="#">
-                        <Input
-                            newType="text"
-                            handleChange={e => {
+                <div className="container">
+                    <div className="box">
+                        <Form
+                            addTask={e => {
+                                e.preventDefault();
+                                const { tasks } = this.state;
+                                let { newTask } = this.state;
+                                newTask = newTask.trim();
+
+                                if (tasks.indexOf(newTask) !== -1) {
+                                    return;
+                                }
+                                const myTasks = [...tasks];
+                                this.setState({
+                                    tasks: [...myTasks, newTask]
+                                });
+                            }}
+                            setTask={e => {
                                 this.setState({
                                     newTask: e.target.value
                                 });
                             }}
-                            msg="Your Task"
+                            newTask={this.state.newTask}
                         />
-                        <Button method="submit">
-                            <FaPlus />
-                        </Button>
-                        <div>{this.state.newTask}</div>
-                    </form>
+                        <ul className="my-tasks">
+                            {this.state.tasks.map((task, index) => (
+                                <li key={index}>
+                                    {task}
+                                    <div>
+                                        <FaEdit
+                                            onClick={e => {
+                                                console.log("Edit", index);
+                                            }}
+                                            className="edit"
+                                        />
+                                        <FaWindowClose
+                                            onClick={e => {
+                                                const { tasks } = this.state;
+                                                const newTasks = [...tasks];
+                                                newTasks.splice(index, 1);
+                                                this.setState({
+                                                    tasks: [...newTasks]
+                                                });
+                                            }}
+                                            className="delete"
+                                        />
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
         );
